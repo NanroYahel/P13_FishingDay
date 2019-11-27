567b5024bc1003c8ede143050a34bd95b6c7c15b
NanroYahel <ronanlehay@gmail.com>
2019-11-27 21:40:44 +0000

    Add template auto-reload env var

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(BASEDIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TEMPLATES_AUTO_RELOAD = True
 
 class ConfigTest(object):
     """Contain the configuration for the tests"""
